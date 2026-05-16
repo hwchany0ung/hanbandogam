44f86e5cbf87695d1227bff6631532c4085157d0
hwchanyung <hwchanyung@gmail.com>
2026-05-16 16:06:36 +0900

    chore(infra): upgrade EC2 t3.small->t3.medium, uvicorn workers 1->2

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -13,7 +13,7 @@ variable "project_name" {
 variable "instance_type" {
   description = "EC2 인스턴스 타입"
   type        = string
-  default     = "t3.small"
+  default     = "t3.medium"
 }
 
 variable "key_pair_name" {
